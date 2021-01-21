0f3ac44ec9ddf323607c29579cd38a0dafdc3a7f
Jeremiah Ang <jeremiahang94@gmail.com>
2021-01-21 17:14:13 +0800

    Code structure

--- App.js
+++ App.js
@@ -3,14 +3,37 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {View, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {View, StatusBar, Text, TextInput, Button} from 'react-native';
+
+function Counter(props) {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <View>
+        <Button title="+"></Button>
+        <Button title="-"></Button>
+      </View>
+    </View>
+  );
+}
+
+function PlusMinusCounter(props) {
+  return (
+    <View>
+      <TextInput></TextInput>
+      <Counter></Counter>
+    </View>
+  );
+}
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View></View>
+      <View>
+        <PlusMinusCounter></PlusMinusCounter>
+      </View>
     </>
   );
 };
